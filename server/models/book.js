73926c8d25b7d8884fb8cd926e41bed2b479ebd4
cofdness <vtkp2002dn@gmail.com>
2019-03-21 20:25:49 +0700

    Prepare steps

---
+++ server/models/book.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const bookSchema = new Schema({
+    isbn: {type: String, required: true, unique: true, trim: true},
+    title: {type: String, required: true, trim: true},
+    author: {type: String, required: true, trim: true},
+    description: {type: String, required: true, trim: true},
+    published_year: {type: Number, min: 1945, max: 2019, required: true},
+    updated_date: { type: Date, default: Date.now}
+})
+const Book = mongoose.model('Book', bookSchema)
+module.exports = Book
